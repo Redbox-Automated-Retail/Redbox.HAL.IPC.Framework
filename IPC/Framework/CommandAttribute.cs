086ed96297c9ffbf4a97fb600ceadf9a77c21978
Redbox Automated Retail LLC <196465444+redbox-automated-retail-llc@users.noreply.github.com>
2025-01-28 23:58:22 -0500

    Init

---
+++ IPC/Framework/CommandAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Redbox.IPC.Framework
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CommandAttribute : Attribute
+    {
+        public CommandAttribute(string name) => this.Name = name;
+
+        public string Name { get; set; }
+    }
+}
---
+++ IPC/Framework/CommandFormAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Redbox.IPC.Framework
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class CommandFormAttribute : Attribute
+    {
+        public string Name { get; set; }
+    }
+}
